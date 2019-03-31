7586bea2905e4eb552ee45f6d2c7ac4a5d3f5b8c
DESKTOP-SCEIRBG\time23 <wh1246935943>
2019-03-31 22:01:34 +0800

    删除注释代码。

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,4 @@
-// const express = require('express')
-// const app = express()
- 
-// var agentsData = require('./src/mock/db.json')  //本地json文件数据
- 
-// var apiRoutes = express.Router();
-// app.use('/api',apiRoutes)
-
 module.exports = {
   publicPath: './',
-  assetsDir: './dist/',
-  // devServer:{
-  //   before(app) {
-  //     app.get('/api/agentsData', (req, res) => {
-  //       res.json(agentsData)
-  //     })
-  //   },
-  //   open: process.platform === 'darwin',
-  //   host: '0.0.0.0',
-  //   port: 8080,
-  //   https: false,
-  //   hotOnly: false,
-  //   proxy: null      //设置跨域，即将本文件内任何没有匹配到的静态文件，都指向跨域地址
-  // }
+  assetsDir: './dist/'
 }
